86aa417afbdcbf0caf011d4c898fef1ed82a1523
OJP98 <ojuarez_p@hotmail.com>
2019-10-02 14:14:36 -0600

    Images in multicommunication slide
    - No parallax effect done, sadly

--- react-project/src/components/Multicommuncation.js
+++ react-project/src/components/Multicommuncation.js
@@ -6,6 +6,11 @@ class Multicommunication extends React.Component {
     {
         const iphoneImg = require('..\\..\\resources\\images\\mobile1iphone.jpg');
         const img1 = require('..\\..\\resources\\images\\img1.png');
+        const img2 = require('..\\..\\resources\\images\\img2.png');
+        const img3 = require('..\\..\\resources\\images\\img3.png');
+        const img4 = require('..\\..\\resources\\images\\img4.png');
+        const img5 = require('..\\..\\resources\\images\\img5.png');
+        const img6 = require('..\\..\\resources\\images\\img6.png');
         return(
             <div className="secondScreen">
                 <h3 className="subtitle">
@@ -18,10 +23,12 @@ class Multicommunication extends React.Component {
                     The fun is always on with 100+ million online users at any time.
                 </p>
                 <img alt="" className="secondScreenIphone" src={iphoneImg}/>
-                {/* <span className="flyImgSpanL1">
-                    <img alt="" src={layer1} className="flyImg" />
-                </span> */}
-                {/* <FloatingImage src={img1} /> */}
+                <img alt="" src={img1} className="flyImg flyImg1" />
+                <img alt="" src={img2} className="flyImg flyImg2" />
+                <img alt="" src={img3} className="flyImg flyImg3" />
+                <img alt="" src={img4} className="flyImg flyImg4" />
+                <img alt="" src={img5} className="flyImg flyImg5" />
+                <img alt="" src={img6} className="flyImg flyImg6" />
             </div>
         );
     }
